b3afa13789177e454f4a3e9c926414aa0e0bd998
Armand Adroher <aadroher@gmail.com>
2021-09-27 23:50:25 +0200

    Adds data for first day, its implementation and day argument.

--- src/Days/Day1.hs
+++ src/Days/Day1.hs
@@ -1,4 +1,17 @@
 module Days.Day1 where
 
+import RIO
+import qualified RIO.Text as T
+
 getFuelRequirements :: Integer -> Integer
 getFuelRequirements n = n `div` 3 - 2
+
+loadFuelRequirements :: FilePath -> IO [Integer]
+loadFuelRequirements f = do
+  fileContents <- readFileUtf8 f
+  pure (read <$> lines (T.unpack fileContents))
+
+calculateResult :: FilePath -> IO Integer
+calculateResult f = do
+  fuelRequirements <- loadFuelRequirements f
+  pure $ sum fuelRequirements
--- test/DaysSpec.hs
+++ test/DaysSpec.hs
@@ -1,15 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module DaysSpec (spec) where
 
 import Import
+import qualified Import as D1
 import Test.Hspec
-import Test.Hspec.QuickCheck
-import Util
 
 spec :: Spec
 spec = do
   describe "day 1" $ do
-    it "should return 2 for 12" $ getFuelRequirements 12 `shouldBe` 2
+    it "should return 2 for 12" $ D1.getFuelRequirements 12 `shouldBe` 2
     it "should return 2 for 14" $ getFuelRequirements 14 `shouldBe` 2
     it "should return 654 for 1969" $ getFuelRequirements 1969 `shouldBe` 654
