bd8119f624d85e431452ff0f85701a47ea67ca30
Armand Adroher <aadroher@gmail.com>
2021-12-03 01:28:51 +0100

    Not so successful attempts

--- src/Days2021/Day1.hs
+++ src/Days2021/Day1.hs
@@ -1,8 +1,44 @@
+{--# LANGUAGE NoImplicitPrelude #-}
+
 module Days2021.Day1 where
 
+import qualified Data.List.Split as S
 import Import
 import qualified RIO.List as L
+import qualified RIO.List.Partial as L'
 import qualified RIO.Text as T
+
+-- data Window = Window
+--   { windowIndex :: Int,
+--     windowReadings :: (Int, Int, Int)
+--   }
+--   deriving (Eq, Ord, Show)
+
+type Window = (Int, (Int, Int, Int))
+
+mkWindow :: [(Int, Int)] -> Maybe Window
+mkWindow [(i, a), (_, b), (_, c)] = Just (i, (a, b, c))
+mkWindow [(i, a), (_, b)] = Just (i, (a, b, 0))
+mkWindow [(i, a)] = Just (i, (a, 0, 0))
+mkWindow _ = Nothing
+
+getWindowSequence :: [(Int, Int)] -> [Window]
+getWindowSequence [] = []
+getWindowSequence xs =
+  let nextWindowData = L'.init $ L.take 4 xs
+   in case mkWindow nextWindowData of
+        Just newWindow -> newWindow : getWindowSequence (L.drop 4 xs)
+        Nothing -> getWindowSequence (L.drop 4 xs)
+
+getWindowSequenceAt :: Int -> [Int] -> [Window]
+getWindowSequenceAt i xs =
+  getWindowSequence (drop i pairs)
+  where
+    pairs = L.zip ([0, 1 ..] :: [Int]) xs
+
+generateWindows :: [Int] -> [Window]
+generateWindows xs =
+  L.sort $ L.concatMap (`getWindowSequenceAt` xs) [0 .. 3]
 
 compareInitialPair :: [Int] -> Int
 compareInitialPair [] = 0
