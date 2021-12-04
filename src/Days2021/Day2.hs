0181e4c9fe363a1dfff956f08f2f9d638842f45f
Armand Adroher <aadroher@gmail.com>
2021-12-04 01:41:48 +0100

    Solves 2.1

--- src/Days2021/Day2.hs
+++ src/Days2021/Day2.hs
@@ -1,14 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
+
 module Days2021.Day2 where
 
 import Import
+import qualified RIO.Text as T
 
 type Position = (Int, Int)
 
 data Command = F Int | U Int | D Int
   deriving (Eq, Show)
 
+parseCommand :: Text -> Command
+parseCommand (T.stripPrefix "forward " -> Just ds) = F (read $ T.unpack ds)
+parseCommand (T.stripPrefix "up " -> Just ds) = U (read $ T.unpack ds)
+parseCommand (T.stripPrefix "down " -> Just ds) = D (read $ T.unpack ds)
+parseCommand _ = undefined
+
 move :: Position -> [Command] -> Position
 move p [] = p
 move (x, y) (F n : cs) = move (x + n, y) cs
 move (x, y) (U n : cs) = move (x, y - n) cs
 move (x, y) (D n : cs) = move (x, y + n) cs
+
+loadCommands :: FilePath -> IO [Command]
+loadCommands f = do
+  fileContents <- readFileUtf8 f
+  let cs = parseCommand <$> T.lines fileContents
+  pure cs
+
+calculateResult :: ([Command] -> Position) -> FilePath -> IO Text
+calculateResult f p = do
+  cs <- loadCommands p
+  let (x, y) = f cs
+  pure $ (T.pack . show) (x * y)
+
+calculateFirstResult :: FilePath -> IO Text
+calculateFirstResult = calculateResult (move (0, 0))
--- src/Run.hs
+++ src/Run.hs
@@ -7,6 +7,7 @@ import qualified Days2019.Day1 as D1of2019
 import qualified Days2019.Day2 as D2of2019
 import qualified Days2019.Day3 as D3of2019
 import qualified Days2021.Day1 as D1of2021
+import qualified Days2021.Day2 as D2of2021
 import Import
 import qualified RIO.List as L
 import qualified RIO.Text as T
@@ -19,6 +20,7 @@ getSolver "19-2-2" = D2of2019.calculateSecondResult
 getSolver "19-3-1" = D3of2019.calculateFirstResult
 getSolver "21-1-1" = D1of2021.calculateFirstResult
 getSolver "21-1-2" = D1of2021.calculateSecondResult
+getSolver "21-2-1" = D2of2021.calculateFirstResult
 getSolver _ = undefined
 
 getFilePath :: String -> FilePath
