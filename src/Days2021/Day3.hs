0ceeba240245b186dbc99f694d903b54ed04e09d
Armand Adroher <aadroher@gmail.com>
2021-12-05 18:44:45 +0100

    WIP

--- src/Days2021/Day3.hs
+++ src/Days2021/Day3.hs
@@ -1,3 +1,71 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Days2021.Day3 where
 
+import Data.Tuple.Select as S
 import Import
+import qualified RIO.List as L
+import qualified RIO.Text as T
+
+data Bit = Zero | One
+  deriving (Eq, Show)
+
+type BinNum = (Bit, Bit, Bit, Bit, Bit)
+
+bitToFloat :: Floating a => Bit -> a
+bitToFloat Zero = 0.0
+bitToFloat One = 1.0
+
+getCommonalityByComparator :: Floating a => (a -> a -> Bool) -> [Bit] -> Bit
+getCommonalityByComparator comp bs =
+  let avg = sum (bitToFloat <$> bs) / fromIntegral (L.length bs)
+   in bool Zero One (avg `comp` 0.5)
+
+getLeastCommon :: [Bit] -> Bit
+getLeastCommon = getCommonalityByComparator (>)
+
+getMostCommon :: [Bit] -> Bit
+getMostCommon = getCommonalityByComparator (<)
+
+getColumnAt :: Int -> [BinNum] -> [Bit]
+getColumnAt i bns = sel <$> bns
+  where
+    sel = case i of
+      0 -> S.sel1
+      1 -> S.sel2
+      2 -> S.sel3
+      3 -> S.sel4
+      4 -> S.sel5
+      _ -> error "Index out of bounds"
+
+calculateGammaRate :: [BinNum] -> BinNum
+calculateGammaRate bs =
+  ( colAgg 0,
+    colAgg 1,
+    colAgg 2,
+    colAgg 3,
+    colAgg 4
+  )
+  where
+    colAgg = \i -> getMostCommon $ getColumnAt i bs
+
+calculateEpsilonRate :: [BinNum] -> BinNum
+calculateEpsilonRate bs =
+  ( colAgg 0,
+    colAgg 1,
+    colAgg 2,
+    colAgg 3,
+    colAgg 4
+  )
+  where
+    colAgg = \i -> getMostCommon $ getColumnAt i bs
+
+parseBit :: Char -> Bit
+parseBit '0' = Zero
+parseBit '1' = One
+parseBit _ = undefined
+
+parseBinNum :: Text -> BinNum
+parseBinNum t = (b0, b1, b2, b3, b4)
+  where
+    [b0, b1, b2, b3, b4] = parseBit <$> T.unpack t
--- test/Days2021Spec.hs
+++ test/Days2021Spec.hs
@@ -6,6 +6,8 @@ module Days2021Spec (spec) where
 import qualified Days2021.Day1 as D1
 import Days2021.Day2 (Command (..))
 import qualified Days2021.Day2 as D2
+import Days2021.Day3 (Bit (..))
+import qualified Days2021.Day3 as D3
 import Import
 import Test.Hspec
 
@@ -35,8 +37,22 @@ spec = do
           D1.countWindowIncreases [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
             `shouldBe` 5
     describe "exercise 2.1" $ do
+      describe "move" $ do
+        it "(0, 0) -> [] -> (0, 0)" $ do
+          D2.stepMove (0, 0) [] `shouldBe` (0, 0)
+        it "(0, 0) -> [F5, D5, F8, U3, D8, F2] -> (15, 10)" $ do
+          D2.stepMove (0, 0) [F 5, D 5, F 8, U 3, D 8, F 2] `shouldBe` (15, 10)
+    describe "exercise 2.2" $ do
       describe "bearingMove" $ do
         it "(0, (0, 0)) -> [] -> (0, (0, 0))" $ do
           D2.bearingMove (0, (0, 0)) [] `shouldBe` (0, (0, 0))
         it "(0, (0, 0)) -> [F5, D5, F8, U3, D8, F2] -> (10, (15, 60))" $ do
           D2.bearingMove (0, (0, 0)) [F 5, D 5, F 8, U 3, D 8, F 2] `shouldBe` (10, (15, 60))
+    describe "exercise 3.1" $ do
+      describe "getMostCommon" $ do
+        it "[0,0,1] -> 0" $ do
+          D3.getMostCommon [Zero, Zero, One] `shouldBe` Zero
+        it "[0,1,1] -> 0" $ do
+          D3.getMostCommon [Zero, One, One] `shouldBe` One
+        it "[0,0,1,1] -> 0" $ do
+          D3.getMostCommon [Zero, Zero, One, One] `shouldBe` Zero
