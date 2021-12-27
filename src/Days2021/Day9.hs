e14ed7469372e4a8ba8f23d5dd271980d60dabe1
Armand Adroher <aadroher@gmail.com>
2021-12-27 12:53:33 +0100

    Adds incorrect implementation. Mistaking cols and rows, as always.

--- src/Days2021/Day9.hs
+++ src/Days2021/Day9.hs
@@ -2,3 +2,90 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Days2021.Day9 where
+
+import Import
+import qualified RIO.List as L
+import RIO.List.Partial ((!!))
+import qualified RIO.Set as S
+
+type FloorMap = [[Int]]
+
+type Point = (Int, Int)
+
+isValid :: FloorMap -> Bool
+isValid [] = True
+isValid [_] = True
+isValid (r0 : r1 : rs) = L.length r0 == L.length r1 && isValid (r1 : rs)
+
+dimensions :: FloorMap -> Maybe (Int, Int)
+dimensions fm =
+  if isValid fm
+    then
+      ( case L.headMaybe fm of
+          Just r -> Just (L.length r, L.length fm)
+          Nothing -> Nothing
+      )
+    else Nothing
+
+inBounds :: Point -> FloorMap -> Bool
+inBounds (i, j) fm = case dimensions fm of
+  Just (n, m) -> 0 <= i && i < n && 0 <= j && j < m
+  Nothing -> False
+
+valueAt :: Point -> FloorMap -> Maybe Int
+valueAt (i, j) fm =
+  if inBounds (i, j) fm
+    then Just $ (fm !! j) !! i
+    else Nothing
+
+neighbours :: Point -> FloorMap -> Set Point
+neighbours (i, j) fm =
+  S.fromList $
+    L.foldl
+      ( \ps p ->
+          case valueAt p fm of
+            Just _ -> p : ps
+            Nothing -> ps
+      )
+      []
+      offsets
+  where
+    offsets =
+      [ (i, j + 1),
+        (i, j - 1),
+        (i + 1, j),
+        (i + 1, j + 1),
+        (i + 1, j - 1),
+        (i - 1, j),
+        (i - 1, j + 1),
+        (i - 1, j - 1)
+      ]
+
+isLowPoint :: Point -> FloorMap -> Bool
+isLowPoint p fm =
+  case valueAt p fm of
+    Just pv -> L.all (> pv) neighbourValues
+    Nothing -> False
+  where
+    neighbourValues =
+      L.foldl
+        ( \nvs n ->
+            case valueAt n fm of
+              Just nv -> nv : nvs
+              Nothing -> nvs
+        )
+        []
+        (neighbours p fm)
+
+lowPoints :: FloorMap -> Set Point
+lowPoints fm =
+  case dimensions fm of
+    Nothing -> S.empty
+    Just (n, m) ->
+      S.fromList $
+        L.filter
+          (`isLowPoint` fm)
+          [ (i, j)
+            | i <- [0 .. (n - 1)],
+              j <- [0 .. (m - 1)]
+          ]
--- test/Day9of2021Spec.hs
+++ test/Day9of2021Spec.hs
@@ -1,10 +1,112 @@
 module Day9of2021Spec where
 
+import qualified Days2021.Day9 as D9
+import qualified RIO.Set as S
 import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "AoC 2021" $ do
     describe "exercise 9.1" $ do
-      it "works!" $ do
-        True `shouldBe` True
+      describe "isValid" $ do
+        it "returns True for a valid map" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0]
+                ]
+          D9.isValid floorMap `shouldBe` True
+        it "returns False for an invalid map" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0, 0]
+                ]
+          D9.isValid floorMap `shouldBe` False
+      describe "dimensions" $ do
+        it "returns dimensions for a valid map" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0]
+                ]
+          D9.dimensions floorMap `shouldBe` Just (2, 3)
+        it "returns Nothing for an invalid map" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0, 0]
+                ]
+          D9.dimensions floorMap `shouldBe` Nothing
+      describe "inBounds" $ do
+        it "returns True for a valid position" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0]
+                ]
+          D9.inBounds (1, 2) floorMap `shouldBe` True
+        it "returns False for an invalid position" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0]
+                ]
+          D9.inBounds (2, 1) floorMap `shouldBe` False
+        it "returns False for an position with negative indexes" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0]
+                ]
+          D9.inBounds (2, -1) floorMap `shouldBe` False
+      describe "valueAt" $ do
+        it "valueAt Just value for a valid position" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0]
+                ]
+          D9.valueAt (1, 2) floorMap `shouldBe` Just 0
+        it "returns Nothing for an invalid position" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0]
+                ]
+          D9.valueAt (2, 1) floorMap `shouldBe` Nothing
+      describe "neighbours" $ do
+        it "valueAt Just value for a valid position" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0]
+                ]
+          D9.neighbours (1, 2) floorMap
+            `shouldBe` S.fromList
+              [ (0, 2),
+                (0, 1),
+                (1, 1)
+              ]
+        it "returns Nothing for an invalid position" $ do
+          let floorMap =
+                [ [0, 0],
+                  [0, 0],
+                  [0, 0]
+                ]
+          D9.neighbours (2, 1) floorMap `shouldBe` S.empty
+      describe "lowPoints" $ do
+        it "solves the example" $ do
+          let floorMap =
+                [ [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+                  [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+                  [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+                  [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+                  [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]
+                ]
+          D9.lowPoints floorMap
+            `shouldBe` S.fromList
+              [ (1, 0),
+                (9, 0),
+                (2, 2),
+                (4, 6)
+              ]
