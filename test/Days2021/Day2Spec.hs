fca0db824d57338ee9994c93139f179082b5be92
Armand Adroher <aadroher@gmail.com>
2022-01-25 01:33:08 +0100

    Some more restructure.

--- test/Days2021/Day2Spec.hs
+++ test/Days2021/Day2Spec.hs
@@ -9,34 +9,33 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "AoC 2021 Day 2" $ do
-    describe "exercise 1" $ do
-      describe "move" $ do
-        it "(0, 0) -> [] -> (0, 0)" $ do
-          stepMove (0, 0) [] `shouldBe` (0, 0)
-        it "(0, 0) -> [F5, D5, F8, U3, D8, F2] -> (15, 10)" $ do
-          stepMove
-            (0, 0)
-            [ F 5,
-              D 5,
-              F 8,
-              U 3,
-              D 8,
-              F 2
-            ]
-            `shouldBe` (15, 10)
-    describe "exercise 2" $ do
-      describe "bearingMove" $ do
-        it "(0, (0, 0)) -> [] -> (0, (0, 0))" $ do
-          bearingMove (0, (0, 0)) [] `shouldBe` (0, (0, 0))
-        it "(0, (0, 0)) -> [F5, D5, F8, U3, D8, F2] -> (10, (15, 60))" $ do
-          bearingMove
-            (0, (0, 0))
-            [ F 5,
-              D 5,
-              F 8,
-              U 3,
-              D 8,
-              F 2
-            ]
-            `shouldBe` (10, (15, 60))
+  describe "exercise 1" $ do
+    describe "move" $ do
+      it "(0, 0) -> [] -> (0, 0)" $ do
+        stepMove (0, 0) [] `shouldBe` (0, 0)
+      it "(0, 0) -> [F5, D5, F8, U3, D8, F2] -> (15, 10)" $ do
+        stepMove
+          (0, 0)
+          [ F 5,
+            D 5,
+            F 8,
+            U 3,
+            D 8,
+            F 2
+          ]
+          `shouldBe` (15, 10)
+  describe "exercise 2" $ do
+    describe "bearingMove" $ do
+      it "(0, (0, 0)) -> [] -> (0, (0, 0))" $ do
+        bearingMove (0, (0, 0)) [] `shouldBe` (0, (0, 0))
+      it "(0, (0, 0)) -> [F5, D5, F8, U3, D8, F2] -> (10, (15, 60))" $ do
+        bearingMove
+          (0, (0, 0))
+          [ F 5,
+            D 5,
+            F 8,
+            U 3,
+            D 8,
+            F 2
+          ]
+          `shouldBe` (10, (15, 60))
--- test/Days2021/Day7Spec.hs
+++ test/Days2021/Day7Spec.hs
@@ -4,39 +4,41 @@
 module Days2021.Day7Spec (spec) where
 
 import Days2021.Day7
+  ( getNaiveOptimalAlignment,
+    getWeightedOptimalAlignment,
+  )
 import Import
 import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "AoC 2021 Day 7" $ do
-    describe "optimalAlignment" $ do
-      it "16,1,2,0,4,2,7,1,2,14 -> (2, 37)" $ do
-        let initialPositions =
-              [ 16,
-                1,
-                2,
-                0,
-                4,
-                2,
-                7,
-                1,
-                2,
-                14
-              ]
-        getNaiveOptimalAlignment initialPositions `shouldBe` (2, 37)
-    describe "optimalAlignment" $ do
-      it "16,1,2,0,4,2,7,1,2,14 -> (5, 168)" $ do
-        let initialPositions =
-              [ 16,
-                1,
-                2,
-                0,
-                4,
-                2,
-                7,
-                1,
-                2,
-                14
-              ]
-        getWeightedOptimalAlignment initialPositions `shouldBe` (5, 168)
+  describe "optimalAlignment" $ do
+    it "16,1,2,0,4,2,7,1,2,14 -> (2, 37)" $ do
+      let initialPositions =
+            [ 16,
+              1,
+              2,
+              0,
+              4,
+              2,
+              7,
+              1,
+              2,
+              14
+            ]
+      getNaiveOptimalAlignment initialPositions `shouldBe` (2, 37)
+  describe "optimalAlignment" $ do
+    it "16,1,2,0,4,2,7,1,2,14 -> (5, 168)" $ do
+      let initialPositions =
+            [ 16,
+              1,
+              2,
+              0,
+              4,
+              2,
+              7,
+              1,
+              2,
+              14
+            ]
+      getWeightedOptimalAlignment initialPositions `shouldBe` (5, 168)
