c7be359810165b658ff0fd1edbc88eb749eb8bcd
Armand Adroher <aadroher@gmail.com>
2022-01-30 19:07:06 +0100

    Adds failing test

--- src/Days2021/Day11.hs
+++ src/Days2021/Day11.hs
@@ -9,6 +9,11 @@ import qualified RIO.Partial as P
 import qualified RIO.Text as T
 
 data Octopus = Level Int | Flash
+  deriving (Eq, Show)
+
+type Row = [Octopus]
+
+type Grid = [Row]
 
 gridSize :: Int
 gridSize = 10
@@ -16,15 +21,19 @@ gridSize = 10
 parseOctopus :: String -> Octopus
 parseOctopus = Level . P.read
 
-parseRow :: Text -> [Octopus]
+parseRow :: Text -> Row
 parseRow t = parseOctopus . (: "") <$> T.unpack t
 
-parseGrid :: Text -> [[Octopus]]
+parseGrid :: Text -> Grid
 parseGrid t =
   if numRowsIsCorrect && numColsIsCorrect
     then parsedRows
-    else error "Incorrect size"
+    else error "Incorrect grid size"
   where
     parsedRows = parseRow <$> T.lines t
     numRowsIsCorrect = L.length parsedRows == gridSize
     numColsIsCorrect = L.all ((== gridSize) . L.length) parsedRows
+
+evolve :: Grid -> Int -> Grid
+evolve g 0 = g
+evolve g n = undefined
--- test/Days2021/Day11Spec.hs
+++ test/Days2021/Day11Spec.hs
@@ -3,8 +3,43 @@
 
 module Days2021.Day11Spec (spec) where
 
-import Import
+import Days2021.Day11
+  ( Octopus (..),
+    evolve,
+    parseRow,
+  )
+import RIO
 import Test.Hspec
 
 spec :: Spec
-spec = undefined
+spec = do
+  describe "exercise 1" $ do
+    describe "evolve" $ do
+      let initialGrid =
+            parseRow
+              <$> [ "5483143223",
+                    "2745854711",
+                    "5264556173",
+                    "6141336146",
+                    "6357385478",
+                    "4167524645",
+                    "2176841721",
+                    "6882881134",
+                    "4846848554",
+                    "5283751526"
+                  ]
+      it "returns the right grid for 3 steps" $ do
+        let expectedGrid =
+              parseRow
+                <$> [ "0050900866",
+                      "8500800575",
+                      "9900000039",
+                      "9700000041",
+                      "9935080063",
+                      "7712300000",
+                      "7911250009",
+                      "2211130000",
+                      "0421125000",
+                      "0021119000"
+                    ]
+        evolve initialGrid 3 `shouldBe` expectedGrid
