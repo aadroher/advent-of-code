dc93ed2b00c793ea1ae6c63e68caa5871c51ea6e
Armand Adroher <aadroher@gmail.com>
2022-12-06 23:35:41 +0100

    Solves first exercise.

--- src/Days2022/Day3.hs
+++ src/Days2022/Day3.hs
@@ -3,11 +3,33 @@
 module Days2022.Day3 where
 
 import Import
+import qualified RIO.List as L
+import qualified RIO.List.Partial as L'
+import qualified RIO.Text as T
+import Util (calculateResult)
 
-newtype Item = Item String
+newtype Item = Item Char
+  deriving (Eq, Show)
+
+getDuplicatedItem :: [Item] -> Item
+getDuplicatedItem items =
+  let midPoint = L.length items `div` 2
+      (firstCompartent, secondCompartment) = L.splitAt midPoint items
+      intersection = L.intersect firstCompartent secondCompartment
+   in L'.head intersection
+
+getItemPriority :: Item -> Int
+getItemPriority (Item c) =
+  let charsWithPrio = L.zip (['a' .. 'z'] ++ ['A' .. 'Z']) [1 ..]
+      [(_, priority)] = L.filter ((c ==) . fst) charsWithPrio
+   in priority
 
 getRepeatedItemsPrioritySum :: [[Item]] -> Int
-getRepeatedItemsPrioritySum = undefined
+getRepeatedItemsPrioritySum iss =
+  L.sum $ getItemPriority . getDuplicatedItem <$> iss
 
 parseContentList :: Text -> [Item]
-parseContentList = undefined
+parseContentList t = Item <$> T.unpack t
+
+calculateFirstResult :: FilePath -> IO Text
+calculateFirstResult = calculateResult parseContentList getRepeatedItemsPrioritySum
--- test/Days2022/Day3Spec.hs
+++ test/Days2022/Day3Spec.hs
@@ -4,12 +4,16 @@
 module Days2022.Day3Spec (spec) where
 
 import Days2022.Day3
-  ( getRepeatedItemsPrioritySum,
+  ( Item (..),
+    getDuplicatedItem,
+    getItemPriority,
+    getRepeatedItemsPrioritySum,
     parseContentList,
   )
 import Import
 import Test.Hspec
 
+itemLists :: [Text]
 itemLists =
   [ "vJrwpWtwJgWrhcsFMMfFFhFp",
     "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
@@ -22,6 +26,16 @@ itemLists =
 spec :: Spec
 spec = do
   describe "exercise 1" $ do
+    describe "parseContentList" $ do
+      it "'ab' -> [Item 'a', Item 'b']" $ do
+        parseContentList "ab" `shouldBe` [Item 'a', Item 'b']
+    describe "getDuplicatedItem" $ do
+      it "'abcb' -> Item 'b'" $ do
+        let parsedList = parseContentList "abcb"
+        getDuplicatedItem parsedList `shouldBe` Item 'b'
+    describe "getItemPriority" $ do
+      it "Item 'C' -> 29" $ do
+        getItemPriority (Item 'C') `shouldBe` 29
     it "calculates the result for the example" $ do
       let parsedLists = parseContentList <$> itemLists
       getRepeatedItemsPrioritySum parsedLists `shouldBe` 157
