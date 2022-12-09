3dac50228f77a1752b02af8972c7711c816063fc
Armand Adroher <aadroher@gmail.com>
2022-12-09 20:28:28 +0100

    Parses cd root.

--- src/Days2022/Day7.hs
+++ src/Days2022/Day7.hs
@@ -6,6 +6,7 @@ import Import
 import qualified RIO.List as L
 import qualified RIO.List.Partial as L'
 import qualified RIO.Text as T
+import Text.Regex.TDFA ((=~))
 import Util (calculateResult)
 
 data DirReference = Root | Parent | Child !Text
@@ -20,5 +21,18 @@ data Node = Dir !Text | File !Int !Text
 data ParsedLine = ParsedCommand !Command | ParsedNode !Node
   deriving (Eq, Show)
 
+parseDirReference :: Text -> DirReference
+parseDirReference l = case l of
+  "$ cd /" -> Root
+  "$ cd .." -> Parent
+  _ -> Child $ T.pack (((T.unpack l =~ ("^\\$ cd .*$" :: String))) :: String)
+
+parseCommand :: Text -> Command
+parseCommand l
+  | (T.unpack l =~ ("^\\$ cd .*$" :: String)) :: Bool = Cd (parseDirReference l)
+  | otherwise = undefined
+
 parseLine :: Text -> ParsedLine
-parseLine = undefined
+parseLine l = case T.unpack l of
+  ('$' : _) -> ParsedCommand (parseCommand l)
+  _ -> undefined
